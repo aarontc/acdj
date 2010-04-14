99c2d410c2a2b423a4b10911892d60930b14d5aa
jazzguitarfreak <jazzguitarfreak@e443b260-af5b-3101-41d7-9d022bc9ec3b>
2010-04-14 04:59:35 +0000

    I have a basic interface now I just support adding files to the table and getting sound support set up.

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,10 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QGridLayout>
-
-class QWidget;
-class ControlWidget;
 
 class MainWindow : public QMainWindow
 {
@@ -13,37 +9,6 @@ class MainWindow : public QMainWindow
 
 public:
 	MainWindow(QWidget *parent = 0);
-	~MainWindow();
-
-
-	//new code
-
-private:
-	ControlWidget * controls;
-	QGridLayout * layout;
-
-//protected:
-//	void closeEvent(QCloseEvent *event);
-
-
-//private:
-//	void createActions ();
-//	void createTrayIcon ();
-//
-//	Ui::MainWindow *ui;
-//	QSystemTrayIcon * stiTrayIcon;
-//
-//	QAction * acnMinimize;
-//	QAction * acnMaximize;
-//	QAction * acnRestore;
-//	QAction * acnQuit;
-//	QMenu * mnuTray;
-//
-//
-//private slots:
-//	void on_actionOpen_File_Disk_triggered();
-// void on_actionExit_Program_triggered();
-//	void on_actionAbout_triggered();
 };
 
 #endif // MAINWINDOW_H
